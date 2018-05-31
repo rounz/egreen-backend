963e46ec9cbc61ac8e93e11bbd758284a8c59672
Ngoc Duy (Yui) Pham <ngocduy.pham@outlook.com>
2018-05-31 14:52:35 +0100

    Do not align on -> for http4s project

--- src/main/scala/com/round/egreen/HelloWorldService.scala
+++ src/main/scala/com/round/egreen/HelloWorldService.scala
@@ -10,7 +10,7 @@ class HelloWorldService[F[_]: Effect] extends Http4sDsl[F] {
 
   val service: HttpService[F] = {
     HttpService[F] {
-      case GET                -> Root / "hello" / name =>
+      case GET -> Root / "hello" / name =>
         Ok(Json.obj("message" -> Json.fromString(s"Hello, $name")))
     }
   }
