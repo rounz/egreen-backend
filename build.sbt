ab17a4372163c379796a87e6b75c21dfaa1b6ee0
Thang Mai <maixuanthang@gmail.com>
2018-06-06 07:30:59 +0000

    Serve frontend

--- build.sbt
+++ build.sbt
@@ -14,6 +14,7 @@ lazy val root = (project in file("."))
       Http4s.blaze,
       Http4s.circe,
       Http4s.dsl,
+      Frontend.build,
       typesafeConfig,
       Scalatest.core % "test",
       Logback.classic
---
+++ src/main/scala/com/round/egreen/http/FrontendHttp.scala
@@ -0,0 +1,25 @@
+// Copyright (c) 2018 Round, Inc.. All rights reserved.
+
+package com.round.egreen.http
+
+import cats.effect.Effect
+import org.http4s._
+import org.http4s.dsl.Http4sDsl
+
+class FrontendHttp[F[_]: Effect] extends Http4sDsl[F] {
+
+  def static(file: String, request: Request[F]): F[Response[F]] =
+    StaticFile
+      .fromResource("/META-INF/resources/webjars/round-inc__egreen-frontend/0.0.4/apps/admin/" + file, Some(request))
+      .getOrElseF(NotFound())
+
+  val service: HttpService[F] = {
+    HttpService[F] {
+      case request @ GET -> Root =>
+        static("index.html", request)
+
+      case request @ GET -> Root / path =>
+        static(path, request)
+    }
+  }
+}
